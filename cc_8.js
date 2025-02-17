738687b35873ee15c5f742155f75bca4558723ec
Gmailvicente04 <vicente.oswald@gmail.com>
2025-02-16 19:19:42 -0500

    "Task 5 - Loan Payment Calculation."

--- cc_8.js
+++ cc_8.js
@@ -35,3 +35,13 @@ function calculateRentalCost(days, carType, insurance = false) {
   }
   calculateRentalCost(3, "Economy", true); // Expected output: "Total Rental Cost: $180"
   calculateRentalCost(5, "Luxury", false); // Expected output: "Total Rental Cost: $500"
+
+  // Task 5 - Loan Payment Calculation
+function calculateLoanPayment(principal, rate, time) {
+    let totalPayment = principal + (principal * rate * time);
+    console.log(`Total Payment: $${totalPayment.toFixed(2)}`);
+  }
+  calculateLoanPayment(1000, 0.05, 2); // Expected output: "Total Payment: $1100.00"
+  calculateLoanPayment(5000, 0.07, 3); // Expected output: "Total Payment: $6050.00"
+
+  
